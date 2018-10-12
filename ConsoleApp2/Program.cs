fd8c163ecb86da0b68c7009507bdfdb8f7bc3da5
KabutoYamato <pedrorm19@gmail.com>
2018-10-12 00:20:26 -0500

    SQLEADOR V1
    Aqui esta la version para probar en produccion, la mayoria de issues fueron tratados

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        public static string Periodo = "3172";
+        public static Dictionary<String, String> Conexion = new Dictionary<string, string>
+        {
+            { "Host","den1.mysql6.gear.host" },
+            { "Port","3306" },
+            { "Database","kristensie" },
+            { "User","kristensie" },
+            { "Pass","Ef6jTY_7_00w" }
+        };
+        public static Dictionary<String, String> Obtener = new Dictionary<String, String>
+        {
+            {"Alumnos",
+                "select distinct " +
+                "A.aluctr, A.alupas, A.alunom, A.aluapp, A.aluapm, CAL.carcve, CAL.calcac, " +
+                "CAL.calsit, P.pdocve, P.pdoini, P.pdoter, A.alucur, A.alunac, A.alucol, " +
+                "A.alucll, A.alunum, A.aluciu, A.alucpo, A.alute1, A.alute2, A.alumai " +
+                "from DALUMN A " +
+                "inner join DCALUM CAL on CAL.aluctr = A.aluctr " +
+                "inner join DPERIO P on P.pdocve = CAL.caling " +
+                "inner join DLISTA C on C.aluctr = A.aluctr " +
+                "where  CAL.calsit = \"1\" and C.pdocve = \"{0}\" " +
+                "order by A.aluctr asc" },
+            {"Calificaciones",
+                "select " +
+                "C.aluctr,M.matnom,C.gpocve,C.liscal,C.lispa1,C.lispa2,C.lispa3,C.lispa4, C.lispa5 " +
+                "from DLISTA C " +
+                "inner join DMATER M on M.matcve = C.matcve " +
+                "where C.pdocve = \"{0}\" and C.aluctr = \"{1}\""},
+            {"Kardex",
+                "select " +
+                "K.aluctr,M.matnom,K.pdocve1,K.karcal,K.tcacve " +
+                "from DKARDE K " +
+                "inner join DMATER M on M.matcve = K.matcve " +
+                "where K.aluctr = \"{0}\""},
+            {"Horario",
+                "select " +
+                "L.aluctr,H.matcve,M.matnom,P.persig,P.perape,P.pernom,H.lunhra,H.marhra,H.miehra,H.juehra,H.viehra " +
+                "from DLISTA L " +
+                "inner join DGRUPO H on H.gpocve = L.gpocve and H.pdocve = L.pdocve and H.matcve = L.matcve " +
+                "inner join DMATER M on M.matcve = H.matcve " +
+                "inner join DPERSO P on P.percve = H.percve " +
+                "where L.pdocve = \"{0}\" and L.aluctr = \"{1}\""}
+        };
+
+        static void Main(string[] args)
+        {
+            DataTable Alumnos;
+            DataTable Calificaciones;
+            DataTable Kardex;
+            DataTable Horario;
+
+            OleDbConnection connectionHandler = new OleDbConnection(
+                @"Provider=VFPOLEDB.1;Data Source=C:\SIE\BD");
+
+            connectionHandler.Open();
+
+            if (connectionHandler.State == ConnectionState.Open)
+            {
+                Alumnos = getInfo(connectionHandler, Obtener["Alumnos"], Periodo);
+                //Alumnos = getInfo(connectionHandler,"select * from DCALUM where aluctr = \"201500120\"","");
+                //imprimirTabla(Alumnos);
+                mysql clean = new mysql(Conexion["Host"], Conexion["Port"], Conexion["Database"], Conexion["User"], Conexion["Pass"]);
+                clean.cleandb();
+                clean.terminar();
+
+                for (int i = 0; i < Alumnos.Rows.Count; i++)
+                {
+                    DataRow Alumno = Alumnos.Rows[i];
+                    String Matricula = Alumno[0].ToString().Trim();
+                    if (!String.IsNullOrWhiteSpace(Matricula))
+                    {
+                        try
+                        {
+                            mysql mybd = new mysql(Conexion["Host"], Conexion["Port"], Conexion["Database"], Conexion["User"], Conexion["Pass"]);
+                                Kardex = getInfo(connectionHandler, Obtener["Kardex"], Matricula);
+                                Calificaciones = getInfo(connectionHandler, Obtener["Calificaciones"], Periodo, Matricula);
+                                Horario = getInfo(connectionHandler, Obtener["Horario"], Periodo, Matricula);
+                                Console.WriteLine("subiendo Alumno " + Matricula);
+                                mybd.subirAlumno(Alumno);
+                                Console.WriteLine("subiendo Kardex");
+                                mybd.subirKardex(Kardex);
+                                Console.WriteLine("subiendo Calificaciones");
+                                mybd.subirCalificaciones(Calificaciones);
+                                Console.WriteLine("subiendo Horario");
+                                mybd.subirHorario(Horario);
+                            mybd.terminar();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                    
+                }
+                connectionHandler.Close();
+                Console.ReadKey();
+            }
+
+            
+        }
+
+
+        public static DataTable getInfo(OleDbConnection connection,String SQL,String var1) {
+            DataTable Info;
+            string mySQL = String.Format(SQL, var1);
+            Info = new DataTable();
+            OleDbCommand MyQuery = new OleDbCommand(mySQL, connection);
+            OleDbDataAdapter DA = new OleDbDataAdapter(MyQuery);
+            DA.Fill(Info);
+            return Info;
+        }
+
+        public static DataTable getInfo(OleDbConnection connection,String SQL, String var1, String var2) {
+            DataTable Info;
+            string mySQL = String.Format(SQL, var1,var2);
+            Info = new DataTable();
+            OleDbCommand MyQuery = new OleDbCommand(mySQL, connection);
+            OleDbDataAdapter DA = new OleDbDataAdapter(MyQuery);
+            DA.Fill(Info);
+            return Info;
+        }
+ 
+
+        public static void imprimirTabla(DataTable t)
+        {
+            int i;
+            for (i = 0; i < t.Rows.Count; i++)
+            {
+                for (int j = 0; j < t.Columns.Count; j++)
+                {
+                    Console.Write(t.Rows[i][j].ToString().Trim() + " ");
+                }
+                Console.WriteLine();
+            }
+            if(i != 0)Console.WriteLine(i);
+        }
+    }
+}
---
+++ ConsoleApp2/mysql.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp2
+{
+    class mysql
+    {
+        string connectionString = "server={0};port={1};database={2};uid={3};pwd={4};SslMode=none";
+        MySqlConnection cnn;
+        public mysql(String server, String Port, String database, String user, String password)
+        {
+            cnn = new MySqlConnection(String.Format(connectionString, server, Port, database, user, password));
+            cnn.Open();
+        }
+
+        public void cleandb() {
+            MySqlCommand cmnd = new MySqlCommand();
+            cmnd.Connection = cnn;
+            cmnd.CommandText = "TRUNCATE TABLE alumnos";
+            cmnd.ExecuteNonQuery();
+            cmnd.CommandText = "TRUNCATE TABLE calificaciones";
+            cmnd.ExecuteNonQuery();
+            cmnd.CommandText = "TRUNCATE TABLE horario";
+            cmnd.ExecuteNonQuery();
+            cmnd.CommandText = "TRUNCATE TABLE kardexes";
+            cmnd.ExecuteNonQuery();
+        }
+
+        public void subirAlumno(DataRow Alumno) {
+            string sql = "INSERT INTO " +
+                "alumnos(matricula,contrasena,nombre,carrera,creditos,situacion,pdo_cve,pdo_ini,pdo_ter,curp,nacimiento,direccion,tel_domicilio,tel_movil,correo) " +
+                "VALUES(@matricula,@contrasena,@nombre,@carrera,@creditos,@situacion,@pdo_cve,@pdo_ini,@pdo_ter,@curp,@nacimiento,@direccion,@tel_domicilio,@tel_movil,@correo)";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@matricula", limpiarData(Alumno,0));
+            cmd.Parameters.AddWithValue("@contrasena", limpiarData(Alumno, 1));
+            cmd.Parameters.AddWithValue("@nombre", String.Format("{0} {1} {2}", limpiarData(Alumno, 2), limpiarData(Alumno, 3), limpiarData(Alumno, 4)));
+            cmd.Parameters.AddWithValue("@carrera", limpiarData(Alumno, 5));
+            cmd.Parameters.AddWithValue("@creditos", limpiarData(Alumno, 6));
+            cmd.Parameters.AddWithValue("@situacion", limpiarData(Alumno, 7));
+            cmd.Parameters.AddWithValue("@pdo_cve", limpiarData(Alumno, 8));
+            cmd.Parameters.AddWithValue("@pdo_ini", limpiarData(Alumno,9).Substring(0,10));
+            cmd.Parameters.AddWithValue("@pdo_ter", limpiarData(Alumno, 10).Substring(0, 10));
+            cmd.Parameters.AddWithValue("@curp", limpiarData(Alumno, 11));
+            cmd.Parameters.AddWithValue("@nacimiento", limpiarData(Alumno, 12).Substring(0,10));
+            String[] direccion = {
+                limpiarData(Alumno, 13),
+                limpiarData(Alumno, 14),
+                limpiarData(Alumno, 15),
+                limpiarData(Alumno, 16),
+                limpiarData(Alumno, 17)
+            };
+            cmd.Parameters.AddWithValue("@direccion", String.Format("{0} {1} {2} {3}, {4}", direccion));
+            cmd.Parameters.AddWithValue("@tel_domicilio", limpiarData(Alumno, 18));
+            cmd.Parameters.AddWithValue("@tel_movil", limpiarData(Alumno, 19));
+            cmd.Parameters.AddWithValue("@correo", limpiarData(Alumno, 20));
+
+            cmd.ExecuteNonQuery();
+
+        }
+
+        public void subirCalificaciones(DataTable Calificaciones) {
+            if (Calificaciones.Rows.Count <= 0)
+            {
+                return;
+            }
+            StringBuilder sql = new StringBuilder(
+                "insert into " +
+                "calificaciones(matricula,nombre_mat,grupo,calificacion,parcial_1,parcial_2,parcial_3,parcial_4,parcial_5) " +
+                "values ");
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = cnn;
+
+            for (int i = 0; i < Calificaciones.Rows.Count; i++) {
+                DataRow Calificacion = Calificaciones.Rows[i];
+                sql.Append(
+                    String.Format("( @m{0},@n{0},@g{0},@c{0},@p1{0},@p2{0},@p3{0},@p4{0},@p5{0}),", i)
+                );
+                cmd.Parameters.AddWithValue("@m" + i, limpiarData(Calificacion, 0));
+                cmd.Parameters.AddWithValue("@n" + i, limpiarData(Calificacion, 1));
+                cmd.Parameters.AddWithValue("@g" + i, limpiarData(Calificacion, 2));
+                cmd.Parameters.AddWithValue("@c" + i, limpiarData(Calificacion, 3));
+                cmd.Parameters.AddWithValue("@p1" + i, limpiarData(Calificacion, 4));
+                cmd.Parameters.AddWithValue("@p2" + i, limpiarData(Calificacion, 5));
+                cmd.Parameters.AddWithValue("@p3" + i, limpiarData(Calificacion, 6));
+                cmd.Parameters.AddWithValue("@p4" + i, limpiarData(Calificacion, 7));
+                cmd.Parameters.AddWithValue("@p5" + i, limpiarData(Calificacion, 8));
+            }
+            sql.Length--;
+            cmd.CommandText = sql.ToString();
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void subirKardex(DataTable Kardex) {
+            if (Kardex.Rows.Count <= 0)
+            {
+                return;
+            }
+            StringBuilder sql = new StringBuilder(
+                "insert into " +
+                "kardexes(matricula,nombre_mat,calificacion,periodo,cuatrimestre) " +
+                "values ");
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = cnn;
+
+            for (int i = 0; i < Kardex.Rows.Count; i++)
+            {
+                DataRow kardex = Kardex.Rows[i];
+                sql.Append(
+                    String.Format("( @m{0},@n{0},@c{0},@p{0},@cu{0}),", i)
+                );
+                cmd.Parameters.AddWithValue("@m" + i, limpiarData(kardex, 0));
+                cmd.Parameters.AddWithValue("@n" + i, limpiarData(kardex, 1));
+                cmd.Parameters.AddWithValue("@c" + i, limpiarData(kardex, 2));
+                cmd.Parameters.AddWithValue("@p" + i, limpiarData(kardex, 3));
+                cmd.Parameters.AddWithValue("@cu" + i, limpiarData(kardex, 4));
+            }
+            sql.Length--;
+            cmd.CommandText = sql.ToString();
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void subirHorario(DataTable Horario) {
+            if (Horario.Rows.Count <= 0) {
+                return;
+            }
+
+            StringBuilder sql = new StringBuilder(
+                "insert into " +
+                "horario(matricula,clave_mat,nombre_mat,profesor,lunes,martes,miercoles,jueves,viernes) " +
+                "values ");
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = cnn;
+
+            for (int i = 0; i < Horario.Rows.Count; i++)
+            {
+                DataRow horario = Horario.Rows[i];
+                sql.Append(
+                    String.Format("( @m{0},@cm{0},@nm{0},@p{0},@lu{0},@ma{0},@mi{0},@ju{0},@vi{0}),", i)
+                );
+
+                String[] profesor = {
+                    limpiarData(horario,3),
+                    limpiarData(horario,4),
+                    limpiarData(horario,5)
+                };
+                cmd.Parameters.AddWithValue("@m" + i, limpiarData(horario, 0));
+                cmd.Parameters.AddWithValue("@cm" + i, limpiarData(horario, 1));
+                cmd.Parameters.AddWithValue("@nm" + i, limpiarData(horario, 2));
+                cmd.Parameters.AddWithValue("@p" + i, String.Format("{0} {1} {2}",profesor).Trim());
+                cmd.Parameters.AddWithValue("@lu" + i, limpiarData(horario, 6));
+                cmd.Parameters.AddWithValue("@ma" + i, limpiarData(horario, 7));
+                cmd.Parameters.AddWithValue("@mi" + i, limpiarData(horario, 8));
+                cmd.Parameters.AddWithValue("@ju" + i, limpiarData(horario, 9));
+                cmd.Parameters.AddWithValue("@vi" + i, limpiarData(horario, 10));
+            }
+            sql.Length--;
+            cmd.CommandText = sql.ToString();
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void terminar() {
+            if (cnn != null)
+            {
+                cnn.Close();
+            }
+        }
+
+
+        public String limpiarData(DataRow row, int index) {
+            return row[index].ToString().Trim();
+        }
+    }
+}
